b6c285a6d813a871cae2ffb484d3652c22b065b7
aeramu <sulam3010@gmail.com>
2020-06-03 06:37:49 +0700

    [alam] refactor resolver account and profile

---
+++ resolver/accountResolver.go
@@ -0,0 +1,104 @@
+package resolver
+
+import(
+	"context"
+	"github.com/aeramu/qiup-server/entity"
+	"github.com/aeramu/qiup-server/service"
+	"github.com/aeramu/qiup-server/repository"
+	"github.com/graph-gophers/graphql-go"
+)
+
+func (r *Resolver) IsEmailAvailable(args struct{
+	Email string
+})(bool){
+	accountRepository := repository.NewAccountRepository()
+	account := accountRepository.GetDataByIndex("email",args.Email)
+	if account == nil {
+		return true
+	} else{
+		return false
+	}
+}
+
+func (r *Resolver) IsUsernameAvailable(args struct{
+	Username string
+})(bool){
+	accountRepository := repository.NewAccountRepository()
+	account := accountRepository.GetDataByIndex("username",args.Username)
+	if account == nil {
+		return true
+	} else{
+		return false
+	}
+}
+
+func (r *Resolver) Login(args struct{
+	Email string
+	Password string
+})(string){
+	accountRepository := repository.NewAccountRepository()
+	account := accountRepository.GetDataByIndex("email",args.Email)
+	if account == nil {
+		account = accountRepository.GetDataByIndex("username",args.Email)
+		if account == nil{
+			return "Email or username not registered"
+		}
+	}
+	if service.Hash(args.Password) != account.Password {
+		return "Wrong password"
+	}
+	return service.GenerateJWT(account.ID)
+}
+
+func (r *Resolver) Register(args struct{
+	Email string
+	Username string
+	Password string
+})(string){
+	accountRepository := repository.NewAccountRepository()
+	if accountRepository.GetDataByIndex("email",args.Email) != nil {
+		return "Email already registered"
+	}
+	if accountRepository.GetDataByIndex("username",args.Username) != nil {
+		return "Username already registered"
+	}
+	account := &entity.Account{
+		ID: service.GenerateUUID(),
+		Email: args.Email,
+		Username: args.Username,
+		Password: service.Hash(args.Password),
+	}
+	accountRepository.PutData(account)
+	return service.GenerateJWT(account.ID)
+}
+
+func (r *Resolver) Account(args struct{
+	ID graphql.ID
+})(*AccountResolver){
+	accountRepository := repository.NewAccountRepository()
+	account := accountRepository.GetDataByIndex("_id",string(args.ID))
+	return &AccountResolver{account}
+}
+
+func (r *Resolver) Me(ctx context.Context)(*AccountResolver){
+	token := ctx.Value("token").(string)
+	accountRepository := repository.NewAccountRepository()
+	account := accountRepository.GetDataByIndex("_id",service.DecodeJWT(token))
+	return &AccountResolver{account}
+}
+
+type AccountResolver struct{
+	account *entity.Account
+}
+func (r *AccountResolver) ID()(graphql.ID){
+	return graphql.ID(r.account.ID)
+}
+func (r *AccountResolver) Email()(string){
+	return r.account.Email
+}
+func (r *AccountResolver) Username()(string){
+	return r.account.Username
+}
+func (r *AccountResolver) Profile()(*ProfileResolver){
+	return &ProfileResolver{r.account.Profile}
+}
--- resolver/resolver.go
+++ resolver/resolver.go
@@ -8,70 +8,6 @@ import(
 	"github.com/graph-gophers/graphql-go"
 )
 
-func (r *Resolver) IsEmailAvailable(args struct{
-	Email string
-})(bool){
-	accountRepository := repository.NewAccountRepository()
-	account := accountRepository.GetDataByIndex("email",args.Email)
-	if account == nil {
-		return true
-	} else{
-		return false
-	}
-}
-
-func (r *Resolver) IsUsernameAvailable(args struct{
-	Username string
-})(bool){
-	accountRepository := repository.NewAccountRepository()
-	account := accountRepository.GetDataByIndex("username",args.Username)
-	if account == nil {
-		return true
-	} else{
-		return false
-	}
-}
-
-func (r *Resolver) Login(args struct{
-	Email string
-	Password string
-})(string){
-	accountRepository := repository.NewAccountRepository()
-	account := accountRepository.GetDataByIndex("email",args.Email)
-	if account == nil {
-		account = accountRepository.GetDataByIndex("username",args.Email)
-		if account == nil{
-			return "Email or username not registered"
-		}
-	}
-	if service.Hash(args.Password) != account.Password {
-		return "Wrong password"
-	}
-	return service.GenerateJWT(account.ID)
-}
-
-func (r *Resolver) Register(args struct{
-	Email string
-	Username string
-	Password string
-})(string){
-	accountRepository := repository.NewAccountRepository()
-	if accountRepository.GetDataByIndex("email",args.Email) != nil {
-		return "Email already registered"
-	}
-	if accountRepository.GetDataByIndex("username",args.Username) != nil {
-		return "Username already registered"
-	}
-	account := &entity.Account{
-		ID: service.GenerateUUID(),
-		Email: args.Email,
-		Username: args.Username,
-		Password: service.Hash(args.Password),
-	}
-	accountRepository.PutData(account)
-	return service.GenerateJWT(account.ID)
-}
-
 func (r *Resolver) EditProfile(ctx context.Context, args struct{
 	Name string
 	Bio string
@@ -97,37 +33,6 @@ func (r *Resolver) UploadImage(args struct{
 	s3Repository := repository.NewS3Repository()
 	url := s3Repository.PutImage(directory)
 	return url
-}
-
-func (r *Resolver) Account(args struct{
-	ID graphql.ID
-})(*AccountResolver){
-	accountRepository := repository.NewAccountRepository()
-	account := accountRepository.GetDataByIndex("_id",string(args.ID))
-	return &AccountResolver{account}
-}
-
-func (r *Resolver) Me(ctx context.Context)(*AccountResolver){
-	token := ctx.Value("token").(string)
-	accountRepository := repository.NewAccountRepository()
-	account := accountRepository.GetDataByIndex("_id",service.DecodeJWT(token))
-	return &AccountResolver{account}
-}
-
-type AccountResolver struct{
-	account *entity.Account
-}
-func (r *AccountResolver) ID()(graphql.ID){
-	return graphql.ID(r.account.ID)
-}
-func (r *AccountResolver) Email()(string){
-	return r.account.Email
-}
-func (r *AccountResolver) Username()(string){
-	return r.account.Username
-}
-func (r *AccountResolver) Profile()(*ProfileResolver){
-	return &ProfileResolver{r.account.Profile}
 }
 
 type ProfileResolver struct{
