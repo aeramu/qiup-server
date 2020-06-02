8b23273363b30fe126b6653f463db8e59e8afaca
aeramu <sulam3010@gmail.com>
2020-06-03 06:34:47 +0700

    [alam] refactor schema.go

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -8,40 +8,6 @@ import(
 	"github.com/graph-gophers/graphql-go"
 )
 
-type Resolver struct{}
-
-var Schema = `
-  	schema{
-		query: Query
-		mutation: Mutation
-  	}
-  	type Query{
-		hello: String!
-		account(id: ID!): Account
-		me: Account!
-	}
-	type Mutation{
-		login(email: String!, password: String!): String!
-		register(email: String!, username: String!, password: String!): String!
-		editProfile(name: String!, bio: String!, profilePhoto: String!, coverPhoto: String!): Account!
-		uploadImage(directory: String!): String!
-		isEmailAvailable(email: String!): Boolean!
-		isUsernameAvailable(username: String!): Boolean!
-	}
-	type Account{
-		id: ID!
-		email: String!
-		username: String!
-		profile: Profile!
-	}
-	type Profile{
-		name: String!
-		bio: String!
-		profilePhoto: String!
-		coverPhoto: String!
-	}
-`
-
 func (r *Resolver) Hello()(string){
 	return "Hello world!"
 }
---
+++ resolver/schema.go
@@ -0,0 +1,35 @@
+package resolver
+
+type Resolver struct{}
+
+var Schema = `
+  	schema{
+		query: Query
+		mutation: Mutation
+  	}
+  	type Query{
+		hello: String!
+		account(id: ID!): Account
+		me: Account!
+	}
+	type Mutation{
+		login(email: String!, password: String!): String!
+		register(email: String!, username: String!, password: String!): String!
+		editProfile(name: String!, bio: String!, profilePhoto: String!, coverPhoto: String!): Account!
+		uploadImage(directory: String!): String!
+		isEmailAvailable(email: String!): Boolean!
+		isUsernameAvailable(username: String!): Boolean!
+	}
+	type Account{
+		id: ID!
+		email: String!
+		username: String!
+		profile: Profile!
+	}
+	type Profile{
+		name: String!
+		bio: String!
+		profilePhoto: String!
+		coverPhoto: String!
+	}
+`
